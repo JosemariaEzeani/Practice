d5f2af0b504d85360b8ac7f027eed05edeaaa3e6
josemariaEzeani <dinakachukwu.ezeani13@alumni.imperial.ac.uk>
2020-06-10 04:37:58 +0100

    more notes

--- core/Bash.Shell.PowerShell/practice.sh
+++ core/Bash.Shell.PowerShell/practice.sh
@@ -35,8 +35,41 @@ done
 # use bc for arbitrary precision calculation
 # use printf instead of echo to format and print data
 
+# declare variables (-x to export)
+# ~ slightly different for associative arrays
+echo arr=() #empty array
+echo arr=(1 2 3) #init array
+arr[2]=3 #overwrite 3rd element
+arr+=(4 5) #append 4 to end
+arr=(-1 0)+arr #append to front
+str=$(1s) #save 2nd output as string
+str=( $(1s) ) #save 2nd output as array of files
+# use of unset requires reseting indices
+unset arr[0] # remove elem
+arr=(${arr[@]}) # new array without missing indices
+echo ${arr[0]} #get 1st element
+echo ${arr[@]} #get all elements
+echo ${!arr[@]} #get array indices
+echo ${#arr[@]} #get array size
+echo ${arr[@]:1:5} #get 5 elements starting from 2nd
+
+# functions
+
+# classes
+
 
 # coreutils.textutils
+
+# awk for text processing
+
+# cut for selecting columns from data
+# head/tail to print first n lines of a file
+# tr to translate, squeeze, and/or delete characters
+# sort + uniq + paste
+
+# grep
+
+# sed
 
 
 # otherutils
