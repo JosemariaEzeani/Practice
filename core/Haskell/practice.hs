b95e812182834232c051eaf89ac99fb705fa8cac
josemariaEzeani <dinakachukwu.ezeani13@alumni.imperial.ac.uk>
2020-06-23 23:42:45 +0100

    hs functions

--- core/Haskell/practice.hs
+++ core/Haskell/practice.hs
@@ -1,3 +1,15 @@
+-- Note: never use tabs, indent further when breaking expr, and line up variables.
+-- Remember: lazy loading...
+-- Local variables: Let and Where  Binding
+
+fancyNine =
+   let x = 4
+       y = 5
+   in x + y -- bottom up
+
+fancySeven = 2 * a + 1 -- where cannot exist here (func def)
+    where a = 3 -- top down
+
 -- Pure Functions (only recursion)
 
 {-| Conversion of Loop to Recursion
@@ -17,6 +29,12 @@ loop n x i =
     then loop n (x*2) (i+1)
     else x
 
+...becomes (with guards)
+
+pow2 n
+   | n == 0	= 1
+   | otherwise	= 2 * (pow2 (n-1))
+
 -}
 
 -- Higher Order Functions
@@ -24,6 +42,22 @@ loop n x i =
 -- Monadic Functions
 
 -- Types
+
+-- Lists (must be homogeneous) -- contrasting tuples
+-- append to List ([] is empty)
+x = [1,2,3] -- shorthand for 1 : 2 : 3 : []
+y = 0 : x -- [0,1,2,3] -- strings are lists of characters
+-- Functions: head, tail, null,... (++ is used for concatenation)
+fst' (a,b) = a -- tuple pattern matching
+
+double [] = [] -- avoid type functions to avoid errors
+double (x : xs) = (2 * x) : (double xs)
+double' nums = case nums of -- for when pattern matching is difficult
+   []		-> []
+   (x : xs)	-> (2 * x) : (double' xs)
+   -- cannot use guards in a case expression
+
+
 
 -- Custom Types
 
