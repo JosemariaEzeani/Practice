4d8230ce93fa0446e439154f76f0a5d731a21438
josemariaEzeani <dinakachukwu.ezeani13@alumni.imperial.ac.uk>
2020-07-04 15:25:18 +0100

    end of Haskell journey

--- core/Haskell/practice.hs
+++ core/Haskell/practice.hs
@@ -1,3 +1,6 @@
+module practice where -- Modules (Foo.Bar.Baz for Foo/Bar/Baz.hs)
+-- Explicit export is possible (even for imported modules)
+
 -- Note: never use tabs, indent further when breaking expr, and line up variables.
 -- Remember: lazy loading...
 -- Local variables: Let and Where  Binding
@@ -68,7 +71,7 @@ map length ["Hello","World","!"] -- Map (works well with partial functions)
 x = [1,2,3] -- shorthand for 1 : 2 : 3 : []
 y = 0 : x -- [0,1,2,3] -- strings are lists of characters
 -- Functions: head, tail, null,... (++ is used for concatenation)
-fst' (a,b) = a -- tuple pattern matching
+fst' (a,b) = a -- tuple pattern matching [_ can be used as a wildcard]
 
 double [] = [] -- avoid type functions to avoid errors
 double (x : xs) = (2 * x) : (double xs)
@@ -77,22 +80,55 @@ double' nums = case nums of -- for when pattern matching is difficult
    (x : xs)	-> (2 * x) : (double' xs)
    -- cannot use guards in a case expression
 
--- Custom Types
+type String = [Char] -- Custom Types
+newtype Identifier = Identifier Int -- with constructor
 
--- Type Classes
+-- records should almost always be avoided
+data Customer = Customer -- can create new with updated record
+{ id :: Identifier -- no shared field names
+, name :: String
+, number :: Int
+} -- not extensible (inheritance)
 
--- IO
+-- alt: Algebraic Data Types [multiple constructors with | ]
+data Customer' = Customer' Identifier String Int
+-- Parameterized types can be used to create/ hold types
+-- Note: deriving with some standard type classes (minus functions)
+-- instance is used to extend and existing type to a new type
 
--- Modules
+-- Type Classes (subclassing is possible)
+class Eq a where
+    (==) :: a -> a -> Bool -- at least one must be implemented
+    (/=) :: a -> a -> Bool
+    -- default implementations (from minimum complete definition)
+    x /= y = not (x == y)
+    x == y = not (x /= y)
 
--- Packages
+-- side effects
+main = putStrLn "Hello World" -- IO Note: () represents data Unit = Unit
+-- do is used to chain IO actions [actually syntactic sugar]
+-- getLine is used to read (<- can only be used in a do-block)
+-- return is a function that creates an IO action
 
--- Cabal
+-- readFile
+-- wrteFile
+-- appendFile
+-- Print
+-- interact
+-- Let
+
+import Data.Set -- use import qualified to avoid naming conflicts
+import Data.Sequence as Seq (empty, size) -- () only type instance
+-- also need to define constructors (e.g., (..) or actual list)
+-- Note: type class instances ignore explicit import (automatically imported)
+import Prelude hiding (map) -- hiding (empty, size) to exclude from import
 
 -- Standard Collections
 
--- Common Monads
-
--- Other monads
+-- Monads/ Monadic Functions
 
 -- Libraries
+
+-- Packages [cabal init > dependencies > configure > build > run > install]
+-- [cabal install cabal-install] Cabal (format) | Hackage (online repository)
+-- use sandboxes for different projects to avoid dependency conflicts
